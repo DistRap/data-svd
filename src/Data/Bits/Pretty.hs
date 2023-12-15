7f81af4eb853366b789331a5318589e5c6f1793d
sorki <srk@48.io>
2023-12-15 14:08:16 +0100

    pretty Data.Bits.Pretty

--- src/Data/Bits/Pretty.hs
+++ src/Data/Bits/Pretty.hs
@@ -15,27 +15,48 @@ module Data.Bits.Pretty
   , showHex32
   ) where
 
-import Data.Int
-import Data.Bits
-import Data.Word
-import Text.Printf
+import Data.Bits (FiniteBits, (.&.), shiftR)
+import Data.Word (Word8, Word16, Word32)
+import Text.Printf (PrintfArg, printf)
+import qualified Data.Bits
 
 -- * Show in base
 
+-- | Format number using binary notation with leading 0b,
+-- padded according to its bit size, with `b` suffix
 showBin :: (PrintfArg t, FiniteBits t) => t -> String
-showBin x = printf ("0b%0" ++ (show $ finiteBitSize x) ++ "b") x
+showBin x =
+  printf
+    ("0b%0"
+    ++ (show $ Data.Bits.finiteBitSize x)
+    ++ "b"
+    )
+    x
 
+-- | Format number using decimal notation with `u` suffix
 showDec :: (PrintfArg t, FiniteBits t) => t -> String
-showDec x = printf ("%0"   ++ (show decSize) ++ "u") x
+showDec x =
+  printf ("%0" ++ (show decSize) ++ "u") x
   where
     decSize :: Int
-    decSize = ceiling $ logBase (10 :: Double) (2 ^ (finiteBitSize x))
+    decSize =
+      ceiling
+      $ logBase
+          (10 :: Double)
+          (2 ^ (Data.Bits.finiteBitSize x))
 
 -- | Format number using hexadecimal notation with leading 0x,
 -- padded according to its bit size, with `X` suffix
 showHex :: (PrintfArg t, FiniteBits t) => t -> String
-showHex x = printf ("0x%0" ++ (show $ finiteBitSize x `div` 4) ++ "X") x
+showHex x =
+  printf
+    ("0x%0"
+    ++ (show $ Data.Bits.finiteBitSize x `div` 4)
+    ++ "X"
+    )
+    x
 
+-- | Format number using hexadecimal notation with leading 0x
 formatHex :: PrintfArg t => t -> String
 formatHex = printf "0x%x"
 
@@ -44,17 +65,20 @@ formatHex = printf "0x%x"
 -- | Print number in binary with bits grouped by `groupSize`
 -- e.g. with `groupSize = 4` we would get `0b0000 1010 0000 0101`
 showBinGroups :: (PrintfArg b, Num b, FiniteBits b) => Int -> b -> String
-showBinGroups groupSize x = unwords $ flip map [gs, gs - 1 ..0] $ \g -> ((printf ("%0" ++ (show groupSize) ++ "b") (mask g x)) :: String)
+showBinGroups groupSize x =
+  unwords
+    $ flip map [gs, gs - 1 ..0]
+    $ \g -> ((printf ("%0" ++ (show groupSize) ++ "b") (mask g x)) :: String)
   where
     mask g n = (2^groupSize - 1) .&. (n `shiftR` (fromIntegral (g * groupSize)))
     gs = sz `div` groupSize
-    sz = fromIntegral $ finiteBitSize x
+    sz = fromIntegral $ Data.Bits.finiteBitSize x
 
 -- * Size of Int
 
 -- | Size of `Int` at the current platform
 platformSizeOfInt :: Int
-platformSizeOfInt = finiteBitSize (0 :: Int)
+platformSizeOfInt = Data.Bits.finiteBitSize (0 :: Int)
 
 -- * Shorthand
 
