028aaa1eb2026f3af4db2d0b7005c196aa24ab9f
sorki <srk@48.io>
2023-12-15 14:21:56 +0100

    pretty Data.SVD.Pretty

--- src/Data/SVD/Pretty.hs
+++ src/Data/SVD/Pretty.hs
@@ -3,26 +3,35 @@
 
 module Data.SVD.Pretty where
 
-import Data.Bits.Pretty
-import Data.Word
-import Data.SVD.Types
-import Text.PrettyPrint.ANSI.Leijen hiding ((<$>))
-import Text.Printf
 import Data.Char (toLower)
-import Data.List (intersperse)
+import Data.SVD.Types
+import Data.Word
+import Text.PrettyPrint.ANSI.Leijen hiding ((<$>))
 
+import qualified Data.Bits.Pretty
+
+ppList :: (a -> Doc) -> [a] -> Doc
 ppList pp x = vcat $ map pp x
 
+ppDevice :: Device -> String
 ppDevice res = displayS (renderPretty 0.4 80 (ppDevice' res)) ""
+
+ppDeviceInfo :: Device -> String
 ppDeviceInfo res = displayS (renderPretty 0.4 80 (ppDeviceInfo' res)) ""
+
+ppPeripheral :: Peripheral -> String
 ppPeripheral res = displayS (renderPretty 0.4 80 (ppPeriph res)) ""
+
+ppRegister :: Register -> String
 ppRegister res = displayS (renderPretty 0.4 80 (ppReg res)) ""
 
+ppDevice' :: Device -> Doc
 ppDevice' Device{..} =
   (red $ string deviceName)
   <$$> indent 2 (ppList ppPeriph devicePeripherals)
 
-ppPeriph periph@Peripheral{..} =
+ppPeriph :: Peripheral -> Doc
+ppPeriph Peripheral{..} =
   hardline
   <> (yellow $ string periphName)
   <+> (white $ ppHex periphBaseAddress)
@@ -30,6 +39,7 @@ ppPeriph periph@Peripheral{..} =
   <$$> indent 2 (ppList ppReg periphRegisters)
   <//> (maybe empty (\x -> string "Derived from" <+> string x) periphDerivedFrom)
 
+ppReg :: Register -> Doc
 ppReg Register{..} =
   hardline
   <> (blue $ string regName)
@@ -37,6 +47,10 @@ ppReg Register{..} =
   <+> (cyan $ char '-' <+> (string regDescription))
   <$$> indent 2 (ppList ppField regFields)
 
+ppIvoryReg
+  :: Peripheral
+  -> Register
+  -> Doc
 ppIvoryReg Peripheral{..} Register{..} =
   hardline
   <> (red $ string $ "-- " ++ periphName ++ periphDescription)
@@ -64,59 +78,73 @@ ppIvoryReg Peripheral{..} Register{..} =
                             upcaseRegName,
                             " = mkBitDataRegNamed (",
                             (toLower <$> periphName),
-                            "_periph_base + ", hexFormat regAddressOffset, ")",
+                            "_periph_base + ", Data.Bits.Pretty.formatHex regAddressOffset, ")",
                             " \"", lowcaseRegName, "\""
                             ])
 
-hexFormat = printf "0x%x"
-ppHex = text . hexFormat
+ppHex :: Int -> Doc
+ppHex = text . Data.Bits.Pretty.formatHex
+
+rpad :: Int -> String -> String
 rpad m xs = take m $ xs ++ repeat ' '
 
+ppField :: Field -> Doc
 ppField Field{..} =
   (green $ string $ rpad 25 (toLower <$> fieldName))
   <+> string "::"
   <+> ppWidthPad 7 fieldBitWidth
   <+> cyan (string $ " -- " ++ fieldDescription)
 
+ppWidth :: Int -> Doc
 ppWidth 1 = string "Bit"
 ppWidth x = string "Bits" <+> int x
 
+ppWidthPad
+  :: Int
+  -> Int
+  -> Doc
 ppWidthPad m 1 = string $ rpad m "Bit"
 ppWidthPad m x = string $ rpad m $ "Bits " ++ show x
 
+-- * ISR
 
--- ISR
-
+ppDevISR :: Device -> String
 ppDevISR res = displayS (renderPretty 0.4 80 (ppDevISR' res)) ""
 
+ppDevISR' :: Device -> Doc
 ppDevISR' Device{..} = (ppList ppPeriphISR devicePeripherals)
 
-ppPeriphISR periph@Peripheral{..} =
+ppPeriphISR :: Peripheral -> Doc
+ppPeriphISR Peripheral{..} =
   indent 2 (ppList ppISR periphInterrupts)
 --  <//> (maybe empty (\x -> string "Derived from" <+> string x) periphDerivedFrom)
---
---
 
-
+ppISRs :: [Interrupt] -> String
 ppISRs res = displayS (renderPretty 0.4 80 (ppList ppISR res)) ""
 
+ppISR :: Interrupt -> Doc
 ppISR Interrupt{..} = indent 2 (
   "|"
   <+> string interruptName
   <> " -- " <> int interruptValue <+> string interruptDescription
   )
 
--- terse output
+-- Terse output
 
+ppDeviceInfo' :: Device -> Doc
 ppDeviceInfo' Device{..} =
   (red $ string deviceName)
   <$$> indent 2 (ppList ppPeriphName devicePeripherals)
 
-ppPeriphName periph@Peripheral{..} = (yellow $ string periphName)
+ppPeriphName :: Peripheral -> Doc
+ppPeriphName Peripheral{..} = (yellow $ string periphName)
 
--- MemMap
+-- * MemMap
 
+ppMemMap :: [(String, String)] -> String
 ppMemMap res = displayS (renderPretty 0.4 80 (ppList ppMem res)) ""
+
+ppMem :: (String, String) -> Doc
 ppMem (addr, periph) =
      name <> " :: Integer"
   </> name
@@ -124,6 +152,7 @@ ppMem (addr, periph) =
   <> string addr
   where name = string (map toLower periph) <> "_periph_base"
 
+shortField :: Field -> String
 shortField Field{..} = unwords [
   fieldName
   , "offset"
@@ -131,13 +160,13 @@ shortField Field{..} = unwords [
   , "width"
   , show fieldBitWidth ]
 
--- |Print currently set (non-zero) fields
+-- | Print currently set (non-zero) fields
 printSetFields :: (Show a, Eq a, Num a) => [(a, Field)] -> String
 printSetFields = unlines . map printSetField . filterSet
 
 printSetField :: (Show a, Eq a, Num a) => (a, Field) -> String
-printSetField (v,f) | fieldBitWidth f == 1 = concat ["Bit ", show (fieldBitOffset f), " ", fieldName f]
-printSetField (v,f) | otherwise = concat [
+printSetField (_, f) | fieldBitWidth f == 1 = concat ["Bit ", show (fieldBitOffset f), " ", fieldName f]
+printSetField (v, f) | otherwise = concat [
     "Bits ["
   , show (fieldBitOffset f)
   , ":"
@@ -147,23 +176,29 @@ printSetField (v,f) | otherwise = concat [
   , fieldName f
   , " value ", show v]
 
--- |Show `Field` with its range, e.g BRR[15:0] (16 bit wide)
+-- | Show `Field` with its range, e.g BRR[15:0] (16 bit wide)
+showField :: Field -> String
 showField f@Field{..} | fieldReserved = "◦" ++ (fieldRange f)
 showField f@Field{..} | otherwise = fieldName ++ (fieldRange f)
 
--- datasheeeet like
+-- | Datasheeeet like
+fieldRange :: Field -> String
 fieldRange Field{..} | fieldBitWidth == 1 = ""
 fieldRange Field{..} | otherwise = concat ["[", show $ fieldBitWidth - 1, ":0]"]
 
--- |Format field value in hex, padded according to `fieldBitWidth`
+-- | Format field value in hex, padded according to `fieldBitWidth`
 hexFieldVal :: (Integral x, Show x) => Field -> x -> String
-hexFieldVal _ 0                         = "0"
+hexFieldVal _ 0 = "0"
 hexFieldVal f x | fieldBitWidth f ==  1 = showBit x
-hexFieldVal f x | fieldBitWidth f <=  8 = showHex (fromIntegral x :: Word8)
-hexFieldVal f x | fieldBitWidth f <= 16 = showHex (fromIntegral x :: Word16)
-hexFieldVal f x | fieldBitWidth f <= 32 = showHex (fromIntegral x :: Word32)
-hexFieldVal f x | otherwise             = showHex (fromIntegral x :: Word64)
-
-showBit 0 = "0"
-showBit 1 = "1"
-showBit x = error $ "Not a bit: " ++ show x
+  where
+    showBit 0 = "0"
+    showBit 1 = "1"
+    showBit y = error $ "Not a bit: " ++ show y
+hexFieldVal f x | fieldBitWidth f <=  8 =
+  Data.Bits.Pretty.showHex (fromIntegral x :: Word8)
+hexFieldVal f x | fieldBitWidth f <= 16 =
+  Data.Bits.Pretty.showHex (fromIntegral x :: Word16)
+hexFieldVal f x | fieldBitWidth f <= 32 =
+  Data.Bits.Pretty.showHex (fromIntegral x :: Word32)
+hexFieldVal _ x | otherwise =
+  Data.Bits.Pretty.showHex (fromIntegral x :: Word64)
